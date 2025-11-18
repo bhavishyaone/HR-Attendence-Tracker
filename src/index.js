bbf0ef9cbf1c3e6b3b364a562f6476f5ad43f0b9
Bhavishya Sharma <bhavishaya32@gmail.com>
2025-11-18 11:43:20 +0530

    added attendence routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoute from "./routes/auth.route.js";
 import employeeRoutes from "./routes/employee.route.js";
 import departmentRoutes from "./routes/department.route.js"
+import attendenceRoutes from "./routes/attendence.route.js"
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use("/auth", authRoute);
 app.use("/employees", employeeRoutes)
 app.use("/departments", departmentRoutes)
+app.use("/attendence",attendenceRoutes)
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
